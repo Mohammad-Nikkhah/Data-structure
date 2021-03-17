63fce5da8b78ca850bb285224ac52186603a320e
Mohammad-Nikkhah <78091800+Mohammad-Nikkhah@users.noreply.github.com>
2021-03-17 23:21:46 +0330

    Update sort.py

--- sort.py
+++ sort.py
@@ -6,7 +6,5 @@ def qsort(inlist):
         lesser = qsort([x for x in inlist[1:] if x < pivot])
         greater = qsort([x for x in inlist[1:] if x >= pivot])
         return lesser + [pivot] + greater
-
-
     
 print(qsort([29, 9, 13, 56, 90, 23, 102]))
